eee61c2d3e1d1bc74b585ad9d1b36a949199bfe6
Fabio Matt <fabio.meihua08@gmail.com>
2020-08-31 17:24:01 -0300

    Atualizar completo

--- src/main/java/com/example/money/api/resource/PessoaResource.java
+++ src/main/java/com/example/money/api/resource/PessoaResource.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -52,9 +54,11 @@ public class PessoaResource {
 	
 	
 	@GetMapping("/{codigo}")
-	public ResponseEntity<Long> buscarPeloCodigo(@PathVariable Long codigo) {
+	public ResponseEntity<Optional<Pessoa>> buscarPeloCodigo(@PathVariable Long codigo) {
+		
+		Optional<Pessoa> pessoa = pessoaRepository.findById(codigo);
 		if (!pessoaRepository.findById(codigo).isEmpty())
-			return ResponseEntity.ok(codigo);
+			return ResponseEntity.ok(pessoa);
 		else
 			return ResponseEntity.notFound().build();
 	} 
@@ -66,6 +70,18 @@ public class PessoaResource {
 			pessoaRepository.deleteById(codigo);
 			
 		}
+	
+	@PutMapping("/{codigo}")
+	public ResponseEntity<Pessoa> atualizar(@PathVariable Long codigo, @Valid @RequestBody Pessoa pessoa){
+		
+		Pessoa pessoaSalva = pessoaRepository.getOne(codigo);
+		
+		BeanUtils.copyProperties(pessoa, pessoaSalva,"codigo");
+		
+		pessoaRepository.save(pessoaSalva);
+		return ResponseEntity.ok(pessoaSalva) ;
+		
+	}
 		
 	}
 	
